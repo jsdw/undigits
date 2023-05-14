31a5f3ea23ba17a0ea62d60244fd8771e5b02d2d
James Wilson <james@jsdw.me>
2023-05-14 12:42:48 +0100

    no base paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  // dont use absolute base paths:
+  base: '',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
